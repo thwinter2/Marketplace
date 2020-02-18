e8f34b8f41f99a2630767d7cd2cfb06815480082
PJ Loheide <pjloheid@ncsu.edu>
2020-02-17 20:41:39 -0500

    Fixes for user.rb

--- market/app/models/user.rb
+++ market/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 	has_one :cart, :class_name => 'Cart', :foreign_key => 'user_id'
-	has_many :items, through :cart
+	has_many :items, through: :cart
 	has_many :feedbacks, :class_name => 'Feedback', :foreign_key => 'user_id'
 	has_one :purchase_histories, :class_name => 'PurchaseHistory', :foreign_key => 'user_id'
-	has_many :credit_cards, :class_name => 'CreditCard', foreign_key => 'user_id'
+	has_many :credit_cards, :class_name => 'CreditCard', :foreign_key => 'user_id'
 	has_one :wishlist, :class_name => 'Wishlist', :foreign_key => 'user_id'
-	has_many :items, through :wishlist
+	has_many :items, through: :wishlist
 end
