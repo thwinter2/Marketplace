6d5dacbb1783f1a107c6e3f2de6065bd733a0b00
PJ Loheide <pjloheid@ncsu.edu>
2020-02-28 21:19:00 -0500

    Item validation

--- market/app/models/item.rb
+++ market/app/models/item.rb
@@ -3,6 +3,16 @@ class Item < ApplicationRecord
 	# belongs_to :wishlist
 	has_many :purchase_histories, :class_name => 'PurchaseHistory', :foreign_key => 'item_id', dependent: :delete_all
 
+	validates :name, presence: true
+	validates :brand, presence: true
+	validates :category, presence: true
+	validates :description, presence: true
+	validates :image, presence: true
+	validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 0}
+	validates :cost, numericality: {greater_than: 0}
+	validates :tax_slab, presence: true
+
+
 	def self.search_category(category)
 		if category != ""
 			categories = Item.where(category: category)
